5c8804899ce6a1382fbcc487bacbd5cef47ba3f8
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-11-04 11:39:42 +0000

    add

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,24 @@
 class TodosController < ApplicationController
+    def index
+        @todos = Todo.all
+    end
+
+    def create
+        @todo = Todo.new(todo_params)
+        if @todo.save
+            redirect_to todos_path
+        else
+            render :index
+        end
+    end
+
+    def show
+        @todo = Todo.find(params[:id])
+    end
+
+    private
+
+    def todo_params
+        params.require(:todo).permit(:title)
+    end
 end
