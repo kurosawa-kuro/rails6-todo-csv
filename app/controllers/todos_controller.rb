1b1812fab7ee40d0542629022bd6d4f6ddb2a0a7
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-11-04 11:52:30 +0000

    add

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class TodosController < ApplicationController
     def index
         @todos = Todo.all
@@ -16,6 +18,32 @@ class TodosController < ApplicationController
         @todo = Todo.find(params[:id])
     end
 
+    def export
+        @todos = Todo.all
+    
+        csv_data = CSV.generate(headers: true) do |csv|
+          csv << ['ID', 'Title'] # CSV Header
+    
+          @todos.each do |todo|
+            csv << [todo.id, todo.title]
+          end
+        end
+    
+        send_data csv_data, filename: "todos-#{Date.today}.csv"
+    end
+
+    def import
+        if params[:file].present?
+          CSV.foreach(params[:file].path, headers: true) do |row|
+            Todo.create(title: row['Title'])
+          end
+          flash[:notice] = "Todos imported successfully!"
+        else
+          flash[:alert] = "Please upload a CSV file."
+        end
+        redirect_to todos_path
+    end
+    
     private
 
     def todo_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'todos#index'
-  resources :todos, only: [:create, :index, :show]
+  resources :todos do
+    collection do
+      get :export, to: "todos#export", as: :export
+      post :import, to: "todos#import", as: :import
+    end
+  end
 end
